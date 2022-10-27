f825d9c734ccb3d179467cb3bd7e9be9eda61fe5
José Reis <josemreis@pm.me>
2022-10-27 11:30:50 +0100

    add packer json file and init bash script

---
+++ scripts/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# clean out the cached package data
+sudo apt-get clean -q
+
+# update and upgrade
+sudo apt-get update -q
+sudo apt-get -y upgrade -q
+
+# installing dependencies
+sudo apt-get install -y libterm-readkey-perl ca-certificates wget curl git expect iproute2 procps libnm0 make npm -qq
+
+# install firefox and xvfb which we will need for openwpm
+echo ""
+echo "[+] Installing firefox and xvfb"
+echo ""
+
+## installing firefox and xvfb
+sudo apt-get install -y firefox xvfb -q
