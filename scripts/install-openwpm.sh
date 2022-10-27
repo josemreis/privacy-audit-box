525a87da478ce11d927156f4259e36714ff7fd18
José Reis <josemreis@pm.me>
2022-10-27 19:01:43 +0100

    expressvpn and openwpm provisions first version added

---
+++ scripts/install-openwpm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e # exit immediatly on error
+set -v # Print commands and their arguments as they are executed.
+set -x
+
+# git clone openwpm
+cd /opt
+
+sudo git clone https://github.com/openwpm/OpenWPM.git
+
+# install conda environment and dependencies
+echo ""
+echo "> Building the openwpm conda environment"
+echo ""
+
+cd OpenWPM/
+source $HOME/miniconda3/etc/profile.d/conda.sh
+# Make conda available to shell script
+eval "$(conda shell.bash hook)"
+if [ "$1" != "--skip-create" ]; then
+    echo 'Creating / Overwriting openwpm conda environment.'
+    # `PYTHONNOUSERSITE` set so python ignores local user site libraries when building the env
+    # See: https://github.com/openwpm/OpenWPM/pull/682#issuecomment-645648939
+    PYTHONNOUSERSITE=True conda env create --force -q -f environment.yaml
+fi
+echo 'Activating environment.'
+conda activate openwpm
+echo 'Installing firefox.'
+./scripts/install-firefox.sh
+echo 'Building extension.'
+./scripts/build-extension.sh
+echo 'Installation complete, activate your new environment by running:'
+echo 'conda activate openwpm'
